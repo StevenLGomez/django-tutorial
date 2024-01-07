3a25b4957312ecc7b58ef44731a9e20c7ad5d244
StevenLGomez <steve_gomez@usa.net>
2024-01-06 21:26:36 -0600

    Code changes for completion of Module 5

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -2,9 +2,14 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from meetings.models import Meeting
+
 # Create your views here.
 def welcome(request):
-    return HttpResponse("Welcome to the Meeting Planner")
+    return render(request, "website/welcome.html",
+        {"num_meetings": Meeting.objects.count()})
+    #    {"message": "This data was sent from the view to the template."})
+    # return HttpResponse("Welcome to the Meeting Planner")
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
